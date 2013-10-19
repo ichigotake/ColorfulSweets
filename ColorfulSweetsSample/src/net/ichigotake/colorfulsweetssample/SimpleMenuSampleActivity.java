b39aab616c0589f1ed30bc63d32f519a1a91f07d
ichigotake <k.wisiiy@gmail.com>
2013-10-19 12:57:07 +0900

    sample code into one file

--- ColorfulSweetsSample/src/net/ichigotake/colorfulsweetssample/SimpleMenuSampleActivity.java
+++ ColorfulSweetsSample/src/net/ichigotake/colorfulsweetssample/SimpleMenuSampleActivity.java
@@ -2,14 +2,18 @@ package net.ichigotake.colorfulsweetssample;
 
 import java.util.List;
 
+import net.ichigotake.colorfulsweets.lib.menu.Menu;
 import net.ichigotake.colorfulsweets.lib.menu.MenuItem;
 import net.ichigotake.colorfulsweets.lib.menu.MenuListAdapter;
 import net.ichigotake.colorfulsweets.lib.menu.SimpleMenuListFactory;
-import net.ichigotake.colorfulsweetssample.menu.SimpleMenuSample;
-import net.ichigotake.colorfulsweetssample.menu.SimpleMenuSample.SimpleMenuSampleOnClickListener;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SimpleMenuSampleActivity extends Activity {
 
@@ -29,4 +33,56 @@ public class SimpleMenuSampleActivity extends Activity {
 		adapter.notifyDataSetChanged();
 	}
 
+	/**
+	 * item list
+	 */
+	private enum SimpleMenuSample implements Menu {
+
+		SMAPLE1(R.string.simple_menu_item_sample1),
+		SMAPLE2(R.string.simple_menu_item_sample2),
+		SMAPLE3(R.string.simple_menu_item_sample3),
+		SMAPLE4(R.string.simple_menu_item_sample4),
+		SMAPLE5(R.string.simple_menu_item_sample5),
+		SMAPLE6(R.string.simple_menu_item_sample6),
+		;
+
+		final private int mTitle;
+		
+		private SimpleMenuSample(int title) {
+			mTitle = title;
+		}
+		
+		@Override
+		public int getLabelName() {
+			return mTitle;
+		}
+
+		@Override
+		public int getIconResource() {
+			return 0; // non icon
+		}
+
+	}
+	
+	/**
+	 * item on click listener
+	 */
+	private class SimpleMenuSampleOnClickListener implements OnClickListener {
+
+		final private Context mContext;
+		
+		public SimpleMenuSampleOnClickListener(Context context) {
+			mContext = context;
+		}
+		@Override
+		public void onClick(View view) {
+			TextView itemView = (TextView) view;
+			String message = new StringBuilder()
+				.append("clicked item: ")
+				.append(itemView.getText().toString())
+				.toString();
+			Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+		}
+		
+	}
 }
