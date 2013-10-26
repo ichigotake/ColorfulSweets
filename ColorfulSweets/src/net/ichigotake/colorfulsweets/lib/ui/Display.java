0f7263bbc3cd2c99e275a5ab7300b9a08a00d918
ichigotake <k.wisiiy@gmail.com>
2013-10-26 12:27:49 +0900

    add Display for density caluculate

---
+++ ColorfulSweets/src/net/ichigotake/colorfulsweets/lib/ui/Display.java
@@ -0,0 +1,26 @@
+package net.ichigotake.colorfulsweets.lib.ui;
+
+import android.app.Activity;
+import android.util.DisplayMetrics;
+
+/**
+ * API level 1
+ * 
+ * Display utilities.
+ */
+public class Display {
+
+	/**
+	 * API level 1
+	 * 
+	 * TODO Add description
+	 * @param activity
+	 * @param pixel
+	 * @return
+	 */
+	public static float calcDensity(Activity activity, int pixel) {
+		DisplayMetrics metrics = new DisplayMetrics();
+		activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+		return pixel * metrics.density;
+	}
+}
