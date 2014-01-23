0cce8d6e63c382ddd45380c2fd74aa8f26c97734
ichigotake <k.wisiiy@gmail.com>
2014-01-24 01:14:55 +0900

    replace to AutoPagingFragment from RunnableAutoPagingContainer

--- example/src/main/java/net/ichigotake/colorfulsweetssample/view/paging/AutoPagingActivity.java
+++ example/src/main/java/net/ichigotake/colorfulsweetssample/view/paging/AutoPagingActivity.java
@@ -1,66 +1,20 @@
 package net.ichigotake.colorfulsweetssample.view.paging;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import net.ichigotake.colorfulsweets.lib.widget.paging.RunnableAutoPagingContainer;
-import net.ichigotake.colorfulsweets.lib.widget.paging.RunnableAutoPagingListener;
+import net.ichigotake.colorfulsweets.lib.fragment.FragmentTransit;
 import net.ichigotake.colorfulsweetssample.BaseActivity;
 import net.ichigotake.colorfulsweetssample.R;
+import net.ichigotake.colorfulsweetssample.fragment.AutoPagingFragmentSampleFragment;
 
 public class AutoPagingActivity extends BaseActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.simple_menu_list);
-
-        ListView listView = (ListView) findViewById(R.id.menu_list);
-        new RunnableAutoPagingContainer<String>()
-                .initialize(listView, new AutoPagingDemoListener(this));
-    }
-
-    private class AutoPagingDemoListener extends RunnableAutoPagingListener<String> {
-
-        final private Context mContext;
-        final private ArrayAdapter<String> mAdapter;
-
-        AutoPagingDemoListener(Context context) {
-            mContext = context;
-            mAdapter = new ArrayAdapter<String>(mContext, R.layout.menu_item);
-        }
-
-        @Override
-        protected Runnable onPagingListener() {
-            return new Runnable() {
-                @Override
-                public void run() {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            final int size = getAdapter().getCount();
-                            for (int i = 0; i < 20; i++) {
-                                getAdapter().add("item " + (size + i));
-                            }
-                            getAdapter().notifyDataSetChanged();
-                            finish();
-                        }
-                    });
-                }
-            };
-        }
-
-        @Override
-        protected int getPerPage() {
-            return 20;
-        }
-
-        @Override
-        protected ArrayAdapter<String> getAdapter() {
-            return mAdapter;
-        }
+        new FragmentTransit(this)
+                .setAddBackStack(false)
+                .toReplace(R.id.content, AutoPagingFragmentSampleFragment.newInstance());
     }
 
 }
---
+++ library/src/main/java/net/ichigotake/colorfulsweets/lib/fragment/AutoPagingFragment.java
@@ -0,0 +1,133 @@
+package net.ichigotake.colorfulsweets.lib.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import net.ichigotake.colorfulsweets.R;
+import net.ichigotake.colorfulsweets.lib.widget.paging.LoadingState;
+import net.ichigotake.colorfulsweets.lib.widget.paging.PagingState;
+
+/**
+ * The Fragment for auto paging container.
+ */
+abstract public class AutoPagingFragment extends BaseFragment {
+
+    private static BaseAdapter sAdapter;
+    private static LoadingState sLoadingState;
+    private static PagingState sPagingParameter;
+
+    protected BaseAdapter getAdapter() {
+        if (sAdapter == null) {
+            sAdapter = createAdapter();
+        }
+        return sAdapter;
+    }
+
+    protected LoadingState getLoadingState() {
+        if (sLoadingState == null) {
+            sLoadingState = new LoadingState();
+        }
+        return sLoadingState;
+    }
+
+    protected PagingState getPagingState() {
+        if (sPagingParameter == null) {
+            sPagingParameter = new PagingState(getPerPage());
+        }
+        return sPagingParameter;
+    }
+
+    abstract protected void onFirstLoading();
+
+    abstract protected void onPaging();
+
+    abstract protected View createView(
+            LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState);
+
+    abstract protected ListView getListView(View view);
+
+    abstract protected BaseAdapter createAdapter();
+
+    abstract protected int getPerPage();
+
+    protected void nextPage() {
+        getPagingState().nextPage();
+    }
+
+    protected View getProgressBarViewGroup(View view) {
+        return LayoutInflater.from(view.getContext())
+                .inflate(R.layout.colorfulsweets_progress_bar, null);
+    }
+
+    protected View getProgressView(View view) {
+        return view.findViewById(R.id.colorfullsweets_progressBar);
+    }
+
+    protected boolean isRequesting() {
+        return sLoadingState.isLoading();
+    }
+
+    protected void finish() {
+        nextPage();
+        getLoadingState().finish();
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                getProgressView(getView()).findViewById(R.id.colorfullsweets_progressBar)
+                        .setVisibility(View.GONE);
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+                             Bundle savedInstanceState) {
+        return createView(inflater, parent, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ListView listView = getListView(getView());
+        listView.addFooterView(getProgressBarViewGroup(getView()));
+        listView.setAdapter(getAdapter());
+        getAdapter().notifyDataSetChanged();
+
+        getLoadingState().start();
+        onFirstLoading();
+        //must do after onFirstLoading()
+        getListView(getView()).setOnScrollListener(new AutoPagingOnScrollListener());
+    }
+
+
+    private class AutoPagingOnScrollListener implements AbsListView.OnScrollListener {
+
+        @Override
+        public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+        }
+
+        @Override
+        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+            if (! getLoadingState().completed() && ! isRequesting()
+                    && (firstVisibleItem + visibleItemCount) == totalItemCount) {
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        getProgressView(getView()).findViewById(R.id.colorfullsweets_progressBar)
+                                .setVisibility(View.VISIBLE);
+                    }
+                });
+                getLoadingState().start();
+                onPaging();
+            }
+        }
+    }
+}
